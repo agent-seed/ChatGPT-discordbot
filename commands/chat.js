181f76c6c00a2cf42b06489ef34c7cc64755001f
Agentseed <admin@agentseed.org>
2023-01-16 16:46:10 -0700

    It still doesn't work, but now it crashes the bot

---
+++ commands/chat.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { apikey } = require('./config.json');
+var prompt_text = 'Write an abstract existentialist poem in the style of black metal lyrics';
+var response = 'totally not an error, I swear';
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('chat')
+		.setDescription('Talk to ChatGPT')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('The input to echo back')),
+	async execute(interaction) {
+		prompt_text = await interaction.options.getUser('message');
+        openai_test()
+        function openai_test() {
+  var output = ''
+  var XMLHttpRequest = require('xhr2');
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://api.openai.com/v1/engines/text-davinci-003/completions");
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer " + apikey);
+
+xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+        console.log('http status code: ' + xhr.status);
+          if (xhr.status == 400) {
+          }else if (xhr.status == 200) {
+            json = JSON.parse(xhr.responseText);
+            response = json.choices[0].text
+            console.log(response);
+          } else if (xhr.status == 401) {
+            response = 'Error 401, you probably need to check your api key'
+          } else {
+            response = 'Other error, Honestly, you should throw out this code and re-write it, I have no idea what the problem is.'
+          }
+      }   response = output;
+     };
+
+  var data = `{
+    "prompt": "${prompt_text}",
+    "temperature": 0.7,
+    "max_tokens": 4000,
+    "top_p": 1,
+    "frequency_penalty": 0.75,
+    "presence_penalty": 0,
+    "stream": false
+  }`;
+  xhr.send(data);
+}
+        setTimeout(function(){
+		interaction.reply(response);}, 2000)
+	},
+};
+// let open_ai_response;
+
