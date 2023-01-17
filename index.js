80bb815ae374e536b34d595bd599ebea7e15ad2f
Cedar DeLacy <seed@tsck.agency>
2023-01-17 09:42:06 -0700

    is no work, reinstalling

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+// const fs = require('node:fs');
+// const path = require('node:path');
+// const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+// client.commands = new Collection();
+// const commandsPath = path.join(__dirname, 'commands');
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-}
+// for (const file of commandFiles) {
+// 	const filePath = path.join(commandsPath, file);
+// 	const command = require(filePath);
+// 	client.commands.set(command.data.name, command);
+// }
 
-client.once(Events.ClientReady, () => {
-	console.log('Ready!');
-});
+// client.once(Events.ClientReady, () => {
+// 	console.log('Ready!');
+// });
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+// client.on(Events.InteractionCreate, async interaction => {
+// 	if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+// 	const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+// 	if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
+// 	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	}
+// });
 const { CommandoClient } = require('discord.js-commando');
-const client = new CommandoClient();
-client.registry
-      .registerDefaultTypes()
-      .registerGroups([
-            ['group1', 'Group 1'],
-            ['group2', 'Group 2'],
-      ])
-      .registerCommands([
-            ChatCommand
-      ]);
+const path = require('path');
+const client = new CommandoClient({
+	commandPrefix: '>',
+	owner: '687855724717801502',
+	invite: 'https://discord.gg/github',
+});
 
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		['first', 'Your First Command Group'],
+		['second', 'Your Second Command Group'],
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+
+	
+	client.once('ready', () => {
+		console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+		client.user.setActivity('with Commando');
+	});
+	
+	client.on('error', console.error);
 
 client.login(token);
